9c1b89f9457a3229aff54c16f30b6dbc20f5d2ea
Lourdes Sanchez <81830540+lourdesanchez@users.noreply.github.com>
2022-08-15 01:49:05 -0300

    Add files via upload

--- imp/js/feedback.js
+++ imp/js/feedback.js
@@ -12,64 +12,32 @@ function recolectarDatos() {
   // PASOS 1 y 2: obtenemos los inputs que tienen los datos que nos interesan, y luego obtenemos el valor (los datos). 
   
   // Nombre e email
-  var inputNombre = document.getElementById("fname");
+  var textareaTexto = document.getElementById("texto");
+  var texto = textareaTexto.value;
+
+  var inputNombre = document.getElementById("nombre");
   var nombre = inputNombre.value;
 
-  var inputMail = document.getElementById("mail");
-  var mail = inputMail.value;
+  // Select de tema
+  var selectTema = document.getElementById("tema");
+  var temaElegido = selectTema.value;
 
-  // Select de autos
-  var selectAutos = document.getElementById("fautos");
-  var autoElegido = selectAutos.value;
-
-  // Checkbox de vehículos
-  var checkboxVehiculo1 = document.getElementById("vehicle1");
-  var valorCheckbox1 = checkboxVehiculo1.checked;
-  var checkboxVehiculo2 = document.getElementById("vehicle2");
-  var valorCheckbox2 = checkboxVehiculo2.checked;
-  var checkboxVehiculo3 = document.getElementById("vehicle3");
-  var valorCheckbox3 = checkboxVehiculo3.checked;
-
-  // Radio button de lenguajes
-  var lenguaje = displayRadioButtonValue();
 
 
   // PASO 3: obtenemos los lugares del html donde nos interesa MOSTRAR los datos que ingresó el usuario
+  var spanTexto = document.getElementById("texto-usuario");
   var spanNombre = document.getElementById("nombre-usuario");
-  var spanEmail = document.getElementById("mail-usuario");
-  var spanAutoElegido = document.getElementById("auto-elegido");
-  var spanTieneBici = document.getElementById("tiene-bici");
-  var spanTieneAuto = document.getElementById("tiene-auto");
-  var spanTieneMoto = document.getElementById("tiene-moto");
-  var spanLenguajeFavorito = document.getElementById("lenguaje-favorito");
   
   // PASO 4: ponemos las variables que obtuvimos en el paso 2, en los elementos html que obtuvimos en el paso 3
+  spanTexto.innerText = texto;
   spanNombre.innerText = nombre;
-  spanEmail.innerText = mail;
-  spanAutoElegido.innerText = autoElegido;
-  if (valorCheckbox1) {
-    spanTieneBici.innerText = "Sí";
-  } else {
-    spanTieneBici.innerText = "No";
-  }
-  if (valorCheckbox2) {
-    spanTieneAuto.innerText = "Sí";
-  } else {
-    spanTieneAuto.innerText = "No";
-  }
-  if (valorCheckbox3) {
-    spanTieneMoto.innerText = "Sí";
-  } else {
-    spanTieneMoto.innerText = "No";
-  }
-  spanLenguajeFavorito.innerText = lenguaje;
 }
 
 function resetearFormulario() {
-  var inputNombre = document.getElementById("fname");
-  var inputMail = document.getElementById("mail");
+  var textareaTexto = document.getElementById("texto");
+  var inputNombre = document.getElementById("nombre");
+  textareaTexto.value = "";
   inputNombre.value = "";
-  inputMail.value = "";
 }
 
 function enviarFormulario(e) {
@@ -87,14 +55,3 @@ function cerrarFeedback() {
   seccionFormulario.classList.remove("no-mostrar");
 }
 
-function displayRadioButtonValue() {
-  var ele = document.querySelectorAll("#radio-container input");
-
-  for (i = 0; i < ele.length; i++) {
-    if ((ele[i].type = "radio")) {
-      if (ele[i].checked) {
-        return ele[i].value;
-      }
-    }
-  }
-}
